33a39f4545c99c903de6ba08049bd74fc5f8a316
nicoletta777 <n.parras23@uniandes.edu.co>
2026-05-13 09:42:52 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
+const opciones = {
+  hair: [
+    "LINK_PELO_1",
+    "LINK_PELO_2"
+  ],
+  outfit: [
+    "LINK_OUTFIT_1",
+    "LINK_OUTFIT_2"
+  ],
+  shoes: [
+    "LINK_ZAPATOS_1",
+    "LINK_ZAPATOS_2"
+  ]
+};
 
+function cambiar(tipo, index) {
+  document.getElementById(tipo).src = opciones[tipo][index];
+}
